93de70aea02f4467dae61bf7405abbe3eef85bad
akukamil <42219788+akukamil@users.noreply.github.com>
2021-03-18 13:41:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ var M_WIDTH=800, M_HEIGHT=450;
 var app, game_res; 
 var spr, user;
 
+
+var callback_users_getCurrentUser = function(method,result,data){
+	if (result) {
+			console.log(result);
+	} else {
+			console.log(data);
+	}
+};
+
 function load()
 {
 	
@@ -10,8 +19,9 @@ function load()
 
 		function() {
 		alert("Инициализация прошла успешно");
+		FAPI.Client.call({"method":"users.getCurrentUser", "fields":"first_name,last_name,location,pic128x128"}, callback_users_getCurrentUser);
 		},
-
+		
 		function(error) {
 		alert("Ошибка инициализации");
 		}
